db8390ae64aefe7074391be4f06a601d3cf6b6d6
luigi <gallo.1895146@studenti.uniroma1.it>
2023-06-05 13:49:39 +0200

    modified similarity net according to paper description

--- models/similarity_net.py
+++ models/similarity_net.py
@@ -1,3 +1,4 @@
+import sys
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -26,60 +27,111 @@ class SimilarityNet(nn.Module):
     '''
         Whole similarity net
     '''
-    def __init__(self, out_channels):
+    def __init__(self, out_channels=64):
         super(SimilarityNet, self).__init__()
         # Initialize resnet34 and import its weights
         self.resnet = Resnet34(block=BasicBlock, layers=[3, 4, 6, 3])
         self.resnet.load_state_dict(resnet34(weights='DEFAULT').state_dict())
 
         # Convolutional layers needed to fix channel size (for both img and ref)
-        self.conv1_img = nn.Conv2d(in_channels=64, out_channels=out_channels, kernel_size=3, padding=1)        
-        self.conv2_img = nn.Conv2d(in_channels=128, out_channels=out_channels, kernel_size=3, padding=1)        
-        self.conv3_img = nn.Conv2d(in_channels=256, out_channels=out_channels, kernel_size=3, padding=1)
-        self.conv4_img = nn.Conv2d(in_channels=512, out_channels=out_channels, kernel_size=3, padding=1)
-        self.conv_img = [self.conv1_img, self.conv2_img, self.conv3_img, self.conv4_img]
-
-        self.conv1_ref = nn.Conv2d(in_channels=64, out_channels=out_channels, kernel_size=3, padding=1)        
-        self.conv2_ref = nn.Conv2d(in_channels=128, out_channels=out_channels, kernel_size=3, padding=1)        
-        self.conv3_ref = nn.Conv2d(in_channels=256, out_channels=out_channels, kernel_size=3, padding=1)
-        self.conv4_ref = nn.Conv2d(in_channels=512, out_channels=out_channels, kernel_size=3, padding=1)
-        self.conv_ref = [self.conv1_ref, self.conv2_ref, self.conv3_ref, self.conv4_ref]
-
-        # Batch normalization and relu
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU()
+        self.layer1 = nn.Sequential(
+            # First convolution after resnet
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1), 
+            nn.InstanceNorm2d(128),
+            nn.PReLU(),
+            # Second convolution (to fix channel size)
+            nn.Conv2d(in_channels=128, out_channels=out_channels, kernel_size=3, padding=1),
+            nn.InstanceNorm2d(out_channels),
+            nn.PReLU()
+        )
+        self.layer2 = nn.Sequential(
+            # First convolution after resnet
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1), 
+            nn.InstanceNorm2d(128),
+            nn.PReLU(),
+            # Second convolution (to fix channel size)
+            nn.Conv2d(in_channels=128, out_channels=out_channels, kernel_size=3, padding=1),
+            nn.InstanceNorm2d(out_channels),
+            nn.PReLU()
+        )
+        self.layer3 = nn.Sequential(
+            # First convolution after resnet
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1), 
+            nn.InstanceNorm2d(256),
+            nn.PReLU(),
+            # Second convolution (to fix channel size)
+            nn.Conv2d(in_channels=256, out_channels=out_channels, kernel_size=3, padding=1),
+            nn.InstanceNorm2d(out_channels),
+            nn.PReLU()
+        )
+        self.layer4 = nn.Sequential(
+            # First convolution after resnet
+            nn.Conv2d(in_channels=512, out_channels=256, kernel_size=3, padding=1), 
+            nn.InstanceNorm2d(256),
+            nn.PReLU(),
+            # Second convolution (to fix channel size)
+            nn.Conv2d(in_channels=256, out_channels=out_channels, kernel_size=3, padding=1),
+            nn.InstanceNorm2d(out_channels),
+            nn.PReLU()
+        )
+        self.layers = [self.layer1, self.layer2, self.layer3, self.layer4]
 
         # Learnable coefficients for weighted feature maps
         self.A = nn.Parameter(torch.ones((2,4,4)))
 
-        # Convolution filter
-        self.conv_filter = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, padding=1) 
+        # Shared convolutional filter
+        self.W_img = nn.Sequential(
+            nn.Conv2d(in_channels=out_channels*4, out_channels=out_channels*4, kernel_size=3, padding=1),
+            nn.InstanceNorm2d(out_channels),
+            nn.PReLU()
+        )
+
+        self.W_ref = nn.Sequential(
+            nn.Conv2d(in_channels=out_channels*4, out_channels=out_channels*4, kernel_size=3, padding=1),
+            nn.InstanceNorm2d(out_channels),
+            nn.PReLU()
+        )
 
 
     def forward(self, x):
         # Extraxt the 4 feature maps using resnet34
         fm_img = self.resnet(x[0].unsqueeze(0))
-        print("Resnet out shape: ", [fm_img[i][0].shape for i in range(4)]) # DEBUG PRINT
+        print("Resnet out img: ", [t.shape for t in fm_img]) # DEBUG PRINT
         fm_ref = self.resnet(x[1].unsqueeze(0))
-        print("Resnet out shape: ", [fm_ref[i][0].shape for i in range(4)]) # DEBUG PRINT
+        print("Resnet out ref: ", [t.shape for t in fm_ref]) # DEBUG PRINT
 
-        # Apply convolution, instace norm, relu
-        fm_img = [self.relu(self.bn(self.conv_img[i](fm_img[i]))) for i in range(4)]
-        print("Conv out shape: ", [fm_img[i][0].shape for i in range(4)]) # DEBUG PRINT
-        fm_ref = [self.relu(self.bn(self.conv_ref[i](fm_ref[i]))) for i in range(4)]
-        print("Conv out shape: ", [fm_ref[i][0].shape for i in range(4)]) # DEBUG PRINT
+        # Apply layers (2 convolutions)
+        fm_img = [self.layers[i](fm_img[i]) for i in range(4)]
+        print("\nConv out img: ", [t.shape for t in fm_img]) # DEBUG PRINT
+        fm_ref = [self.layers[i](fm_ref[i]) for i in range(4)]
+        print("Conv out ref: ", [t.shape for t in fm_ref]) # DEBUG PRINT
 
-        # Compute M weighted concatenations
-        M_img = [self.conv_filter(torch.cat([self.A[0,i,j]*F.interpolate(fm_img[j], size=fm_img[i].shape[-2:], mode='bilinear') for j in range(4)])) for i in range(4)]
-        print("M shapes: ", [M_img[i].shape for i in range(4)]) # DEBUG PRINT
-        M_ref = [self.conv_filter(torch.cat([self.A[1,i,j]*F.interpolate(fm_ref[j], size=fm_ref[i].shape[-2:], mode='bilinear') for j in range(4)])) for i in range(4)]
-        print("M shapes: ", [M_ref[i].shape for i in range(4)]) # DEBUG PRINT
+        # Compute M weighted concatenations and apply convolution to it
+        M_img = [self.W_img(torch.cat([self.A[0,i,j]*F.interpolate(fm_img[j], size=fm_img[i].shape[-2:], mode='bilinear') for j in range(4)], dim=1)) for i in range(4)]
+        print("\nM img: ", [t.shape for t in M_img]) # DEBUG PRINT
+        M_ref = [self.W_ref(torch.cat([self.A[1,i,j]*F.interpolate(fm_ref[j], size=fm_ref[i].shape[-2:], mode='bilinear') for j in range(4)], dim=1)) for i in range(4)]
+        print("M res: ", [t.shape for t in M_ref]) # DEBUG PRINT
 
-        # Compute similarity maps
-        a = [M_img[i]-torch.mean(M_img[i]) for i in range(4)]
-        b = [M_ref[i]-torch.mean(M_ref[i]) for i in range(4)]
-        sm = [(a[i] * b[i]) / (torch.norm(a[i], p=2) * torch.norm(b[i], p=2)) for i in range(4)]
+        # Reshape from 3D to 2D (from HxWxC to HWxC)
+        M_img = [M_img[i].view(1, M_img[i].shape[-3], -1) for i in range(4)]
+        print("\nM reshaped img: ", [t.shape for t in M_img]) # DEBUG PRINT
+        M_ref = [M_ref[i].view(1, M_ref[i].shape[-3], -1) for i in range(4)]
+        print("M reshaped ref: ", [t.shape for t in M_ref]) # DEBUG PRINT
 
+        # Compute M - mean(M)
+        I = [M_img[i]-M_img[i].mean(dim=-1, keepdim=True) for i in range(4)]
+        R = [M_ref[i]-M_img[i].mean(dim=-1, keepdim=True) for i in range(4)]
 
+        # Compute norm of (M - mean(M)) + epsilon
+        norm_I = [torch.norm(I[i], p=2, dim=1, keepdim=True) + sys.float_info.epsilon for i in range(4)]
+        norm_R = [torch.norm(R[i], p=2, dim=1, keepdim=True) + sys.float_info.epsilon for i in range(4)]
+
+        # Compute ((Mi - mean(Mi))*(Mr - mean(Mr))) / (norm(Mi - mean(Mi)) * (norm(Mr - mean(Mr)))
+        sm = [torch.matmul(torch.div(I[i], norm_I[i]).permute(0, 2, 1), torch.div(R[i], norm_R[i])).unsqueeze(dim=1) for i in range(4)]
+
+        # Reshape to (WxH) (same W,H size of feature maps)
+        sm = [torch.max(sm[i], dim=-1, keepdim=True)[0] for i in range(4)]
+        sm = [sm[i].view(1, 1, fm_img[i].shape[-2],  fm_img[i].shape[-1]) for i in range(4)]
+        print("\nSM: ", [t.shape for t in sm])
 
         return sm
