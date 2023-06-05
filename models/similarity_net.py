6006689ee70dfbbe0494856fd1b18b0e2230d163
luigi <gallo.1895146@studenti.uniroma1.it>
2023-06-05 21:29:10 +0200

    added similarity map demo to notebook. deleted test file notebook

--- models/similarity_net.py
+++ models/similarity_net.py
@@ -5,6 +5,8 @@ import torch.nn.functional as F
 from torchvision.models import resnet34
 from torchvision.models.resnet import ResNet, BasicBlock
 
+DEBUG = True
+
 
 class Resnet34(ResNet):
     '''
@@ -44,6 +46,7 @@ class SimilarityNet(nn.Module):
             nn.InstanceNorm2d(out_channels),
             nn.PReLU()
         )
+
         self.layer2 = nn.Sequential(
             # First convolution after resnet
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1), 
@@ -54,6 +57,7 @@ class SimilarityNet(nn.Module):
             nn.InstanceNorm2d(out_channels),
             nn.PReLU()
         )
+
         self.layer3 = nn.Sequential(
             # First convolution after resnet
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1), 
@@ -64,6 +68,7 @@ class SimilarityNet(nn.Module):
             nn.InstanceNorm2d(out_channels),
             nn.PReLU()
         )
+
         self.layer4 = nn.Sequential(
             # First convolution after resnet
             nn.Conv2d(in_channels=512, out_channels=256, kernel_size=3, padding=1), 
@@ -74,12 +79,13 @@ class SimilarityNet(nn.Module):
             nn.InstanceNorm2d(out_channels),
             nn.PReLU()
         )
+
         self.layers = [self.layer1, self.layer2, self.layer3, self.layer4]
 
         # Learnable coefficients for weighted feature maps
         self.A = nn.Parameter(torch.ones((2,4,4)))
 
-        # Shared convolutional filter
+        # Shared convolutional filters
         self.W_img = nn.Sequential(
             nn.Conv2d(in_channels=out_channels*4, out_channels=out_channels*4, kernel_size=3, padding=1),
             nn.InstanceNorm2d(out_channels),
@@ -96,42 +102,42 @@ class SimilarityNet(nn.Module):
     def forward(self, x):
         # Extraxt the 4 feature maps using resnet34
         fm_img = self.resnet(x[0].unsqueeze(0))
-        print("Resnet out img: ", [t.shape for t in fm_img]) # DEBUG PRINT
+        if DEBUG: print("Resnet out img: ", [t.shape for t in fm_img]) # DEBUG PRINT
         fm_ref = self.resnet(x[1].unsqueeze(0))
-        print("Resnet out ref: ", [t.shape for t in fm_ref]) # DEBUG PRINT
+        if DEBUG: print("Resnet out ref: ", [t.shape for t in fm_ref]) # DEBUG PRINT
 
         # Apply layers (2 convolutions)
         fm_img = [self.layers[i](fm_img[i]) for i in range(4)]
-        print("\nConv out img: ", [t.shape for t in fm_img]) # DEBUG PRINT
+        if DEBUG: print("\nConv out img: ", [t.shape for t in fm_img]) # DEBUG PRINT
         fm_ref = [self.layers[i](fm_ref[i]) for i in range(4)]
-        print("Conv out ref: ", [t.shape for t in fm_ref]) # DEBUG PRINT
+        if DEBUG: print("Conv out ref: ", [t.shape for t in fm_ref]) # DEBUG PRINT
 
         # Compute M weighted concatenations and apply convolution to it
         M_img = [self.W_img(torch.cat([self.A[0,i,j]*F.interpolate(fm_img[j], size=fm_img[i].shape[-2:], mode='bilinear') for j in range(4)], dim=1)) for i in range(4)]
-        print("\nM img: ", [t.shape for t in M_img]) # DEBUG PRINT
+        if DEBUG: print("\nM img: ", [t.shape for t in M_img]) # DEBUG PRINT
         M_ref = [self.W_ref(torch.cat([self.A[1,i,j]*F.interpolate(fm_ref[j], size=fm_ref[i].shape[-2:], mode='bilinear') for j in range(4)], dim=1)) for i in range(4)]
-        print("M res: ", [t.shape for t in M_ref]) # DEBUG PRINT
+        if DEBUG: print("M res: ", [t.shape for t in M_ref]) # DEBUG PRINT
 
         # Reshape from 3D to 2D (from HxWxC to HWxC)
         M_img = [M_img[i].view(1, M_img[i].shape[-3], -1) for i in range(4)]
-        print("\nM reshaped img: ", [t.shape for t in M_img]) # DEBUG PRINT
+        if DEBUG: print("\nM reshaped img: ", [t.shape for t in M_img]) # DEBUG PRINT
         M_ref = [M_ref[i].view(1, M_ref[i].shape[-3], -1) for i in range(4)]
-        print("M reshaped ref: ", [t.shape for t in M_ref]) # DEBUG PRINT
+        if DEBUG: print("M reshaped ref: ", [t.shape for t in M_ref]) # DEBUG PRINT
 
-        # Compute M - mean(M)
+        # Compute (M - mean(M)) and it's norm + epsilon
         I = [M_img[i]-M_img[i].mean(dim=-1, keepdim=True) for i in range(4)]
         R = [M_ref[i]-M_img[i].mean(dim=-1, keepdim=True) for i in range(4)]
-
-        # Compute norm of (M - mean(M)) + epsilon
         norm_I = [torch.norm(I[i], p=2, dim=1, keepdim=True) + sys.float_info.epsilon for i in range(4)]
         norm_R = [torch.norm(R[i], p=2, dim=1, keepdim=True) + sys.float_info.epsilon for i in range(4)]
 
         # Compute ((Mi - mean(Mi))*(Mr - mean(Mr))) / (norm(Mi - mean(Mi)) * (norm(Mr - mean(Mr)))
         sm = [torch.matmul(torch.div(I[i], norm_I[i]).permute(0, 2, 1), torch.div(R[i], norm_R[i])).unsqueeze(dim=1) for i in range(4)]
 
-        # Reshape to (WxH) (same W,H size of feature maps)
+        # Apply max along x-axis
         sm = [torch.max(sm[i], dim=-1, keepdim=True)[0] for i in range(4)]
+
+        # Reshape to (WxH) (same W,H size of feature maps)
         sm = [sm[i].view(1, 1, fm_img[i].shape[-2],  fm_img[i].shape[-1]) for i in range(4)]
-        print("\nSM: ", [t.shape for t in sm])
+        if DEBUG: print("\nSM: ", [t.shape for t in sm])
 
         return sm
